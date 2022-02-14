47c7e7bc85724180a514eea54b7b0a760087887e
bjrichards <braedenrichards@nevada.unr.edu>
2022-02-14 15:48:00 -0800

    modified to work with typing linter

--- frogger/player.py
+++ frogger/player.py
@@ -1,4 +1,3 @@
-from turtle import right
 import pygame
 import settings
 
@@ -6,16 +5,18 @@ import settings
 class Player(pygame.sprite.Sprite):
     def __init__(
         self, pos, groups, collision_sprites, up_key, down_key, left_key, right_key
-    ):
+    ) -> None:
         super().__init__(groups)
-        self.image = pygame.Surface((settings.PLAYER_WIDTH, settings.PLAYER_HEIGHT))
+        self.image: pygame.Surface = pygame.Surface(
+            (settings.PLAYER_WIDTH, settings.PLAYER_HEIGHT)
+        )
         self.image.fill(settings.PLAYER_COLOR)
-        self.rect = self.image.get_rect(center=pos)
+        self.rect: pygame.rect.Rect = self.image.get_rect(center=pos)
 
-        self.up_key = up_key
-        self.down_key = down_key
-        self.left_key = left_key
-        self.right_key = right_key
+        self.up_key: int = up_key
+        self.down_key: int = down_key
+        self.left_key: int = left_key
+        self.right_key: int = right_key
 
         self.up_key_bool: bool = False
         self.down_key_bool: bool = False
@@ -23,26 +24,29 @@ class Player(pygame.sprite.Sprite):
         self.right_key_bool: bool = False
 
         self.is_on_log: bool = False
-        self.log_position = (0, 0)
+        self.log_position: tuple = (0, 0)
 
-        self.collision_sprites = collision_sprites
+        self.collision_sprites: pygame.sprite.Group = collision_sprites
+
+        self.is_dead: bool = False
 
         # player movement
         self.direction = pygame.math.Vector2()
         self.speed = settings.TILE_HEIGHT
 
-    def input(self):
+    def input(self) -> None:
         keys = pygame.key.get_pressed()
 
+        # UP/DOWN movement
         if keys[self.up_key]:
             if not self.up_key_bool:
-                self.direction.y = -1  # type:ignore
+                self.direction.y = -1
                 self.up_key_bool = True
             else:
                 self.direction.y = 0
         elif keys[self.down_key]:
             if not self.down_key_bool:
-                self.direction.y = 1  # type:ignore
+                self.direction.y = 1
                 self.down_key_bool = True
             else:
                 self.direction.y = 0
@@ -51,15 +55,16 @@ class Player(pygame.sprite.Sprite):
             self.down_key_bool = False
             self.direction.y = 0
 
+        # LEFT/RIGHT movement
         if keys[self.left_key]:
             if not self.left_key_bool:
-                self.direction.x = -1  # type:ignore
+                self.direction.x = -1
                 self.left_key_bool = True
             else:
                 self.direction.x = 0
         elif keys[self.right_key]:
             if not self.right_key_bool:
-                self.direction.x = 1  # type:ignore
+                self.direction.x = 1
                 self.right_key_bool = True
             else:
                 self.direction.x = 0
@@ -68,28 +73,28 @@ class Player(pygame.sprite.Sprite):
             self.right_key_bool = False
             self.direction.x = 0
 
-    def rectify_position(self):
-        if self.rect.top < 0:  # type:ignore
-            self.rect.center = (  # type:ignore
-                self.rect.center[0],  # type:ignore
-                settings.TILE_HEIGHT / 2,
-            )  # type:ignore
-        elif self.rect.bottom > settings.SCREEN_HEIGHT:  # type:ignore
-            self.rect.center = (  # type:ignore
-                self.rect.center[0],  # type:ignore
-                (settings.SCREEN_HEIGHT - settings.TILE_HEIGHT / 2),
-            )  # type:ignore
-
-        if self.rect.left < 0:  # type:ignore
-            self.rect.center = (  # type:ignore
-                settings.TILE_WIDTH / 2,
-                self.rect.center[1],  # type:ignore
+    def rectify_position(self) -> None:
+        if self.rect.top < 0:
+            self.rect.center = (
+                int(self.rect.center[0]),
+                int(settings.TILE_HEIGHT / 2),
             )
-        elif self.rect.right > settings.SCREEN_WIDTH:  # type:ignore
-            self.rect.center = (  # type:ignore
-                (settings.SCREEN_WIDTH - settings.TILE_WIDTH / 2),
-                self.rect.center[1],  # type:ignore
-            )  # type:ignore
+        elif self.rect.bottom > settings.SCREEN_HEIGHT:
+            self.rect.center = (
+                int(self.rect.center[0]),
+                int(settings.SCREEN_HEIGHT - settings.TILE_HEIGHT / 2),
+            )
+
+        if self.rect.left < 0:
+            self.rect.center = (
+                int(settings.TILE_WIDTH / 2),
+                int(self.rect.center[1]),
+            )
+        elif self.rect.right > settings.SCREEN_WIDTH:
+            self.rect.center = (
+                int(settings.SCREEN_WIDTH - settings.TILE_WIDTH / 2),
+                int(self.rect.center[1]),
+            )
 
         # Rectify horizontal position
         if not self.is_on_log:
@@ -111,6 +116,7 @@ class Player(pygame.sprite.Sprite):
 
     def collisions(self):
         for sprite in self.collision_sprites.sprites():
+            sprite: pygame.Sprite.sprite = sprite
             if sprite.rect.colliderect(self.rect):
                 if sprite.type == "log":
                     self.is_on_log = True
@@ -124,8 +130,8 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
-        self.rect.center += self.direction * self.speed  # type:ignore
+        self.rect.center += self.direction * self.speed  # type: ignore
         self.collisions()
         if self.is_on_log:
-            self.rect.center = self.log_position  # type:ignore
+            self.rect.center = self.log_position
         self.rectify_position()
